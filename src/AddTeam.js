857336d1b66e7606560eb4932ebe2c38466c847f
Spiewakl <ukispiewak@gmail.com>
2023-08-28 19:34:41 +0200

    working add teams

--- src/AddTeam.js
+++ src/AddTeam.js
@@ -12,11 +12,25 @@ import { loginDispatcher } from './Utils/loginDispatcher.utils';
 import lbrm from './Images/lbrm.png';
 import './ImageCenter.css';
 import { listTeam } from './Utils/listTeam.utils';
+import { newTeam } from './Utils/addTeam.utils';
 
 
 function AddTeam(){
   const navigate = useNavigate();
- 
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const handleChange = event => {
+    setLogin(event.target.value);
+
+    console.log('value is:', event.target.value);
+  
+  };
+  const handleChangePassword = event => {
+    setPassword(event.target.value);
+
+    console.log('value is:', event.target.value); 
+  
+  };
     return (
       <div>
       <div className="center-container">
@@ -25,10 +39,10 @@ function AddTeam(){
         <div>
         <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
     
-          <MDBInput wrapperClass='mb-4' label='Podaj nowy identyfikator zespołu' id='form1' type='text'/>
-          <MDBInput wrapperClass='mb-4' label='Podaj nowe hasło zespołu' id='form2' type='password'/>
+          <MDBInput  value={login} onChange={handleChange} wrapperClass='mb-4' label='Podaj nowy identyfikator zespołu' id='form1' type='text'/>
+          <MDBInput value={password} onChange={handleChangePassword} wrapperClass='mb-4' label='Podaj nowe hasło zespołu' id='form2' type='password'/>
         
-          <MDBBtn onClick={() => loginDispatcher(navigate)} className="mb-4">Potwierdź</MDBBtn>
+          <MDBBtn onClick={() => newTeam(navigate, login, password)} className="mb-4">Potwierdź</MDBBtn>
      
     
         </MDBContainer>
--- src/Utils/addTeam.utils.js
+++ src/Utils/addTeam.utils.js
@@ -1,3 +1,14 @@
 export function addTeam(navigate){
     navigate('/addteam')
   }
+ 
+export async function newTeam(navigate, login, password){
+  const data={
+    "name": login,
+    "password": password,
+  }
+
+  const token = window.localStorage.getItem("token");
+    const result = await fetch(`http://localhost:4000/Team/`,{method: "POST", body: JSON.stringify(data), headers: {"Content-Type":"application/json", "Authorization": "Bearer " + token}})
+    navigate('/dispatcher')
+}
